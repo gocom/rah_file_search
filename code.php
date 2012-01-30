0e1cb287e131b50b1783924acbd171e1c3536357
Jukka Svahn <web@rahinaa.biz>
2012-01-30 21:16:47 +0200

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -1,27 +1,79 @@
 <?php	##################
 	#
 	#	rah_file_search-plugin for Textpattern
-	#	version 0.1
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
 	###################
 
-	function rah_file_search($atts) {
-		extract(lAtts(array(
-			'form' => 'files'
-		),$atts));
-		$out = array();
-		$q = doSlash(gps('q'));
-		if($q) {
-			$rs = safe_rows_start("id",'txp_file',"(filename rlike '$q' or description rlike '$q') and status = '4'");
-			while($a = nextRow($rs)) {
-				extract($a);
-				$out[] = file_download(array(
-					'id' => $id,
-					'form' => $form
-				));
-			}
-		}
-		return implode('',$out);
-	} ?>
+/**
+	The tag. Returns the list of matching files.
+*/
+
+	function rah_file_search($atts, $thing = NULL) {
+		
+		global $pretext, $has_article_tag;
+		
+		if(empty($pretext['q']))
+			return;
+		
+		/*
+			We are an article tag,
+			well kinda.
+		*/
+		
+		$has_article_tag = true;
+		$q = $pretext['q'];
+		
+		/*
+			Quotes should be stripped
+			from quote surrounded string
+		*/
+		
+		$quoted = ($q[0] === '"') && ($q[strlen($q)-1] === '"');
+		$q = doSlash($quoted ? trim(trim($q, '"')) : $q);
+		
+		/*
+			Clean whitespace and escape the
+			special syntax MySQL's like operator uses
+		*/
+		
+		$q = 
+			preg_replace('/\s+/', ' ', 
+				str_replace(
+					array('\\','%','_','\''),
+					array('\\\\','\\%','\\_', '\\\''),
+					$q
+				)
+			);
+		
+		/*
+			Get matching IDs
+		*/
+		
+		$rs = 
+			safe_rows(
+				'id',
+				'txp_file',
+				"(filename like '%$q%' or description like '%$q%') and status=4"
+			);
+		
+		if(!$rs)
+			return;
+		
+		$ids = array();
+		
+		foreach($rs as $a) 
+			$ids[] = $a['id'];
+		
+		/*
+			Return the file list
+		*/
+		
+		$atts['id'] = implode(',',$ids);
+		
+		return
+			file_download_list($atts, $thing);
+	}
+?>
